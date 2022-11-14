f79b892d6d08d2d5072890d358d00a4655768b0d
parlorsky <69785001+parlorsky@users.noreply.github.com>
2022-11-14 04:35:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -202,13 +202,13 @@ if st.button('Предсказать зарплату'):
     inputs = [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45, a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64, a65, a66, a67, a68, a69, a70, a71, a72, region_, a74]
     
     if experience == 'Нет опыта':
-        prediction = model_2_code_experience.predict(inputs)
+        prediction = model_no_code_experience.predict(inputs)
     if experience == 'От 1 года до 3 лет':
         prediction = model_0_code_experience.predict(inputs)
     if experience == 'От 3 до 6 лет':
         prediction = model_1_code_experience.predict(inputs)
     if experience == 'Более 6 лет':
-        prediction = model_no_code_experience.predict(inputs)
+        prediction = model_2_code_experience.predict(inputs)
         
         
     print("final pred", np.squeeze(prediction, -1))
