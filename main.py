eeaf021d2441ba74ab90bf6a4d251f519f0d6c8b
parlorsky <69785001+parlorsky@users.noreply.github.com>
2022-11-14 14:16:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ left_column1, right_column1 = st.columns(2)
 with left_column1:
     experience = st.radio(
         'опыт работы:',
-        np.unique(['До года', 'От 1 года до 3 лет','от 3 до 6 лет']))
+        np.unique(['Без опыта', 'От 1 года до 3 лет','от 3 до 6 лет']))
 
 
 a0 = 1 if st.checkbox(cols[0]) else 0
@@ -188,7 +188,7 @@ region_ = data[str(option)]
 if st.button('Предсказать зарплату'):
     inputs = [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45, a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64,region_ ,a65]
     
-    if experience == 'До года':
+    if experience == 'Без опыта':
         prediction = model_0_code_experience.predict(inputs)
     if experience == 'От 1 до 3 лет':
         prediction = model_1_code_experience.predict(inputs)
