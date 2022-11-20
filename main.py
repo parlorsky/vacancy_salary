1a0a1a881f07566cec964f5e511c9ebf938b6d68
parlorsky <tffriend015@gmail.com>
2022-11-21 00:58:00 +0300

    2134

--- main.py
+++ main.py
@@ -309,7 +309,7 @@ else:
             (list(data.keys())))
         a33 = data[str(option)]
         
-        inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33])[model_1_code_experience_is_vahta_sorted_mask]
+        inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33])[model_2_code_experience_is_vahta_sorted_mask]
         prediction = model_2_code_experience_is_vahta.predict(inputs)
     else:
         flag = 5
@@ -354,7 +354,7 @@ else:
         a33 = data[str(option)]
 
         
-        inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33])[model_1_code_experience_isnt_vahta_sorted_mask]
+        inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33])[model_2_code_experience_isnt_vahta_sorted_mask]
         prediction = model_2_code_experience_isnt_vahta.predict(inputs)
 
 
