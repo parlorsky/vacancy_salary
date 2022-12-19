9fd5781997d8cf9bb4d17a1ea8d38ea9601f4db4
parlorsky <tffriend015@gmail.com>
2022-12-19 03:10:05 +0300

    12313

--- main.py
+++ main.py
@@ -669,7 +669,7 @@ else:
 
         a20 = data[str(option)]
 
-        inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31])[model_0_code_experience_sorted_mask]
+        inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31])
         prediction = model_0_code_experience.predict(inputs)
     
 
