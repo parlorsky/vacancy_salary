28512f23ddf6fe030b8a9055e34ffceb8f0d270d
parlorsky <tffriend015@gmail.com>
2022-12-23 00:44:54 +0300

    534512

--- main.py
+++ main.py
@@ -324,7 +324,7 @@ elif inp_species == 'сварщик':
         a5 =   1 if st.checkbox(model_0_code_experience_sorted[5]) else 0
         a6 =   1 if st.checkbox(model_0_code_experience_sorted[6]) else 0
         a7 =   1 if st.checkbox(model_0_code_experience_sorted[7]) else 0
-        a8 =   1 if st.checkbox(model_0_code_experience_sorted[8]) else 0
+        # a8 =   1 if st.checkbox(model_0_code_experience_sorted[8]) else 0
         a9 =   1 if st.checkbox(model_0_code_experience_sorted[9]) else 0
         a10 = 1 if st.checkbox(model_0_code_experience_sorted[10]) else 0
         a11 = 1 if st.checkbox(model_0_code_experience_sorted[11]) else 0
@@ -341,14 +341,14 @@ elif inp_species == 'сварщик':
         a22 = 1 if st.checkbox(model_0_code_experience_sorted[22]) else 0
         a23 = 1 if st.checkbox(model_0_code_experience_sorted[23]) else 0
         a24 = 1 if st.checkbox(model_0_code_experience_sorted[24]) else 0
-        # a25 = 1 if st.checkbox(model_0_code_experience_sorted[23]) else 0
+        a25 = 1 if st.checkbox(model_0_code_experience_sorted[25]) else 0
 
         st.subheader("Выберите регион вакансии")
         option = st.selectbox(
             'Напишите регион вакансии',
             (list(data.keys())))
 
-        a25 = data[str(option)]
+        a8 = data[str(option)]
 
         inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22,a23,a24,a25])[model_0_code_experience_sorted_mask]
         prediction = model_0_code_experience.predict(inputs)
