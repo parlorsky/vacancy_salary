2949f52129581b6fa3ab2ef778b7fdc90c89f9e6
parlorsky <tffriend015@gmail.com>
2022-12-23 00:34:20 +0300

    321321312

--- main.py
+++ main.py
@@ -329,6 +329,7 @@ elif inp_species == 'сварщик':
         a11 = 1 if st.checkbox(model_0_code_experience_sorted[11]) else 0
         a12 = 1 if st.checkbox(model_0_code_experience_sorted[12]) else 0
         a13 = 1 if st.checkbox(model_0_code_experience_sorted[13]) else 0
+        a14 = 1 if st.checkbox(model_0_code_experience_sorted[14]) else 0
         a15 = 1 if st.checkbox(model_0_code_experience_sorted[15]) else 0
         a16 = 1 if st.checkbox(model_0_code_experience_sorted[16]) else 0
         a17 = 1 if st.checkbox(model_0_code_experience_sorted[17]) else 0
@@ -344,9 +345,9 @@ elif inp_species == 'сварщик':
             'Напишите регион вакансии',
             (list(data.keys())))
 
-        a25 = data[str(option)]
+        a23 = data[str(option)]
 
-        inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22,a23,a24,a25,a26])[model_0_code_experience_sorted_mask]
+        inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22,a23])[model_0_code_experience_sorted_mask]
         prediction = model_0_code_experience.predict(inputs)
     
 
