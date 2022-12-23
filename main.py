8157edd604d8b71fb85e231cef34c4f2ab128d86
parlorsky <tffriend015@gmail.com>
2022-12-23 03:21:46 +0300

    3131

--- main.py
+++ main.py
@@ -838,8 +838,8 @@ elif inp_species == 'слесарь':
             a11 = 1 if st.checkbox(model_2_code_experience_n_sorted[11]) else 0
             a12 = 1 if st.checkbox(model_2_code_experience_n_sorted[12]) else 0
             a13 = 1 if st.checkbox(model_2_code_experience_n_sorted[13]) else 0
-            # a14 = 1 if st.checkbox(model_2_code_experience_n_sorted[14]) else 0
-            a15 = 1 if st.checkbox(model_2_code_experience_n_sorted[15]) else 0
+            a14 = 1 if st.checkbox(model_2_code_experience_n_sorted[14]) else 0
+            # a15 = 1 if st.checkbox(model_2_code_experience_n_sorted[15]) else 0
             a16 = 1 if st.checkbox(model_2_code_experience_n_sorted[16]) else 0
             a17 = 1 if st.checkbox(model_2_code_experience_n_sorted[17]) else 0
             a18 = 1 if st.checkbox(model_2_code_experience_n_sorted[18]) else 0
@@ -850,7 +850,7 @@ elif inp_species == 'слесарь':
                 'Напишите регион вакансии',
                 (list(data.keys())))
 
-            a14 = data[str(option)]
+            a15 = data[str(option)]
 
             inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18])[model_2_code_experience_n_sorted_mask]
             prediction = model_2_code_experience_n.predict(inputs)
