3315b1e9cfbab8761c0620fd33dc734ea6cdcd35
parlorsky <tffriend015@gmail.com>
2022-12-23 06:09:30 +0300

    41312

--- main.py
+++ main.py
@@ -1791,6 +1791,7 @@ elif inp_species == 'продавец':
         a11 = 1 if st.checkbox(model_0_code_experience_sorted[11]) else 0
         a12 = 1 if st.checkbox(model_0_code_experience_sorted[12]) else 0
         a13 = 1 if st.checkbox(model_0_code_experience_sorted[13]) else 0
+        a14 = 1 if st.checkbox(model_0_code_experience_sorted[14]) else 0
         a15 = 1 if st.checkbox(model_0_code_experience_sorted[15]) else 0
         a16 = 1 if st.checkbox(model_0_code_experience_sorted[16]) else 0
         a17 = 1 if st.checkbox(model_0_code_experience_sorted[17]) else 0
