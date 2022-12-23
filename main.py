ceaeeb76c9e0f0838faff89d3f94e3b7a1b6c719
parlorsky <tffriend015@gmail.com>
2022-12-23 03:28:08 +0300

    312312

--- main.py
+++ main.py
@@ -667,7 +667,7 @@ elif inp_species == 'слесарь':
 
             a8 = data[str(option)]
 
-            inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18])[model_1_code_experience_sorted_y_mask]
+            inputs = np.array([a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18])[model_1_code_experience_y_sorted_mask]
             prediction = model_1_code_experience_y.predict(inputs)
 
 
