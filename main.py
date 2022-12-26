84cffe7b3d4c041374c3538b5927e185c67b4f7f
parlorsky <tffriend015@gmail.com>
2022-12-26 07:02:54 +0300

    312312

--- main.py
+++ main.py
@@ -384,7 +384,9 @@ elif inp_species == 'сварщик':
             reg = model_1_code_experience_y_sorted_obl[option]
             inputs += [reg]
             prediction = bases[2] + sum(inputs)
-            st.write(prediction)
+            st.write(reg)
+            st.write(inputs)
+            st.write(model_1_code_experience_y_sorted_obl)
 
 
         else:
